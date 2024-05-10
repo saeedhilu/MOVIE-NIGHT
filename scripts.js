11940145c670da0f4736b5022922d7e3836dbab5
SAEED NM <saeedmoozhikkal@gmail.com>
2024-05-10 14:55:12 +0530

    commit after adding photo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".swiper", {
+        effect: "cards",
+        grabCursor: true,
+        initialSlide: 2,
+        speed: 500,
+        loop: true,
+        rotate: true,
+        mousewheel: {
+            invert: false,
+        },
+    });
+});
